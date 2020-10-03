12e48cf2448caeaa9628870831b730a6c45cb55c
Karel Patlejch <patlejch.karel@gmail.com>
2020-10-03 18:18:30 +0100

    fixed viewpager not working after view recreation in home fragment due to using old adapter

--- app/src/main/java/com/patlejch/timetables/ui/home/HomeFragment.kt
+++ app/src/main/java/com/patlejch/timetables/ui/home/HomeFragment.kt
@@ -27,7 +27,7 @@ class HomeFragment : AppFragment<HomeViewModel, FragmentHomeBinding>() {
 
         viewPager = binding.homeViewPager
         viewPager.offscreenPageLimit = 2
-        adapter = adapter ?: DailyTimetablesAdapter(viewModel.date.value, this).also {
+        adapter = DailyTimetablesAdapter(viewModel.date.value, this).also {
             viewPager.adapter = it
             viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
                 override fun onPageSelected(position: Int) {
