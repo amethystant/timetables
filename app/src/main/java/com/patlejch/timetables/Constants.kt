ddb258bd00fe491a0a9a452465b8dcda19259aed
Karel Patlejch <patlejch.karel@gmail.com>
2020-12-17 14:57:34 +0000

    updated url to use https since an ssl certificate has been set up for the domain

--- app/src/main/java/com/patlejch/timetables/Constants.kt
+++ app/src/main/java/com/patlejch/timetables/Constants.kt
@@ -7,8 +7,7 @@ object Constants {
     const val BETA = BuildConfig.BUILD_TYPE == "beta"
     const val RELEASE = BuildConfig.BUILD_TYPE == "release"
 
-    // todo change back to https once you get your CA set up
-    val API_URL = "http://timetables.patlejch.co.uk"
+    val API_URL = "https://timetables.patlejch.co.uk"
 
     const val DB_FORMAT_WHOLE = "yyyyMMdd'T'HHmmss'Z'"
     const val DB_FORMAT_DAY = "yyyyMMdd"
